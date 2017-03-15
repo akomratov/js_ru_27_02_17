a826fbae0399d85fa0354d082b8dcefc854d0b2d
Roman <kaboom.ua@gmail.com>
2017-03-15 23:08:17 +0200

    Update articlesFilter.js

--- src/reducer/articlesFilter.js
+++ src/reducer/articlesFilter.js
@@ -1,5 +1,6 @@
 import {SET_ARTICLES_FILTER} from '../constants'
 
+//не дроби редюсеры слишком сильно: объедини селект и календарь в один
 export default (state = [], action) => {
 
     const { type, payload } = action
@@ -7,6 +8,7 @@ export default (state = [], action) => {
     switch (type) {
         case SET_ARTICLES_FILTER:
             console.log('reducer-articlesFilter.js: payload.selected = ', payload.selected)
+            //здесь лучше просто id хранить, а не все что в Select приходит
             return payload.selected
     }
 

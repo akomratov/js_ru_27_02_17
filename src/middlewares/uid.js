977605e344a5ddff85d060f9bbb94c0569b76528
Roman <kaboom.ua@gmail.com>
2017-03-22 22:57:45 +0200

    Update uid.js

--- src/middlewares/uid.js
+++ src/middlewares/uid.js
@@ -3,7 +3,9 @@ export default store => next => action => {
 
     const {type, payload} = action
 
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
     if(type === ADD_COMMENT) {
+        //и лучше не мутировать payload, мало-ли что там станут передавать
         payload.comment.commentId = parseInt('xxxxx'.replace(/[x]/g, c => Math.random()*9|0))
     }
 
